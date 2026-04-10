5cf0d7183570aa0facee94de699d5463ed056d69
Víctor Enrique Aguirre <68919985+kikewebcity@users.noreply.github.com>
2026-04-10 12:18:08 -0600

    Update Nosotros.jsx

--- src/Components/Nosotros.jsx
+++ src/Components/Nosotros.jsx
@@ -114,7 +114,7 @@ const Nosotros = () => {
 
         <div className="pilar-tarjeta">
           <div className="pilar-icono-caja">
-            <img src="/equipo.png" alt="Trabajo en equipo" />
+            <img src="/equipo.webp" alt="Trabajo en equipo" />
           </div>
           <h2 className="pilar-titulo-valor">TRABAJO EN EQUIPO</h2>
           <p className="pilar-descripcion">
