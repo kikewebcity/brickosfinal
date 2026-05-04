2cdada09c1170bb046fc834bc3d7f6b73481a7a9
Víctor Enrique Aguirre <68919985+kikewebcity@users.noreply.github.com>
2026-05-04 11:48:47 -0600

    Actualizar FaqsProductos.jsx

--- src/Components/FaqsProductos.jsx
+++ src/Components/FaqsProductos.jsx
@@ -7,25 +7,25 @@ const FaqsProductos = () => {
   const faqs = [
     {
       id: 1,
-      pregunta: '1. COMPORTAMIENTO DEL MATERIAL ANTE LA HUMEDAD',
+      pregunta: '1. Comportamiento del material ante la humedad',
       respuesta:
         'Bricko se ha diseñado con un enfoque riguroso para aplicaciones en interiores. Debido a su naturaleza higroscópica, el material debe permanecer resguardado de la precipitación pluvial y medio ambiente exterior. La exposición directa al agua compromete la estabilidad estructural de la matriz de celulosa, por lo que su uso óptimo se limita estrictamente a ambientes secos y controlados.',
     },
     {
       id: 2,
-      pregunta: '2. SEGURIDAD Y RESISTENCIA ANTE EL FUEGO',
+      pregunta: '2. Seguridad y resistencia ante el fuego',
       respuesta:
         'La composición química de la pasta base, fundamentada en enlaces poliméricos de amilopectina, garantiza una alta resistencia al fuego. Al ser sometido a calor extremo, el bloque no propaga la llama; en su lugar, experimenta un proceso de carbonización gradual. Esta característica es una ventaja crítica para la seguridad en desarrollos habitacionales y laborales.',
     },
     {
       id: 3,
-      pregunta: '3. REQUERIMIENTOS DE HERRAMIENTA PARA INSTALACIÓN',
+      pregunta: '3. Requerimientos de herramientas para la instalación',
       respuesta:
         'La maleabilidad es una de las mayores ventajas competitivas de nuestra patente. No se requiere maquinaria pesada ni técnicos especializados para su manejo. El material permite ser taladrado, clavado o atornillado con herramientas manuales comunes, y puede cortarse con precisión milimétrica utilizando una segueta convencional, lo que reduce drásticamente el ruido en obra.',
     },
     {
       id: 4,
-      pregunta: '4. CAPACIDAD DE ABSORCIÓN DE ACABADOS',
+      pregunta: 'Capacidad de absorción de acabados',
       respuesta:
         'La textura rústica y plana de los bloques está diseñada para integrarse estéticamente con facilidad. La superficie soporta la aplicación directa de pinturas vinílicas de base agua o pastas decorativas. Al eliminar por completo los tiempos de fraguado de mezclas húmedas tradicionales, el proceso de acabado final es inmediato y logísticamente eficiente.',
     },
